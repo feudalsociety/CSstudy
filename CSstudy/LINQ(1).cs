4dc54b8b4b736cc598f810d479bd6b197fe7237e
feudalsociety <32698816+feudalsociety@users.noreply.github.com>
2022-03-01 17:10:00 +0900

    second

---
+++ CSstudy/LINQ(1).cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSstudy
+{
+    public enum ClassType
+    {
+        Knight, Archer, Mage
+    }
+
+    public class Player
+    { 
+        public ClassType ClassType { get; set; }
+        public int Level { get; set; }
+        public int Hp { get; set; }
+        public int Attack { get; set; }
+    }
+
+    internal class Program
+    {
+        static List<Player> _players = new List<Player>();
+
+        static void Main(string[] args)
+        {
+
+            Random rand = new Random();
+
+            for (int i = 0; i < 100; i++)
+            {
+                ClassType type = ClassType.Knight;
+                switch (rand.Next(0, 3))
+                { 
+                    case 0:
+                        type = ClassType.Archer;
+                        break;
+
+                    case 1: 
+                        type = ClassType.Mage;
+                        break;
+
+                    case 2:
+                        type = ClassType.Knight;
+                       break;
+                }
+
+                Player player = new Player()
+                {
+                    ClassType = type,
+                    Level = rand.Next(1, 100),
+                    Hp = rand.Next(100, 1000),
+                    Attack = rand.Next(5, 50)
+                };
+
+                _players.Add(player);
+            }
+
+            // 일반 버전
+            // 0) 레벨이 50 이상인 Knight만 추려내서, 레벨을 오름차순 정렬
+            {
+                List<Player> players = GetHighLevelKnights();
+                foreach (Player p in players)
+                {
+                    Console.WriteLine($"{p.Level}, {p.Hp}");
+                }
+            }
+
+            // LINQ 버전 from (Foreach), where 필터역할
+            // orderby 기본적으로 ascending 
+            // select 최종 결과 추출 -> 가공해서 추출도 가능
+            {
+                var players =
+                    from p in _players
+                    where p.ClassType == ClassType.Knight && p.Level >= 50
+                    orderby p.Level // ascending, descending
+                    select p;
+                // select new {Hp = p.Hp, Level = p.Level * 2 };
+
+                foreach (Player p in players)
+                {
+                    Console.WriteLine($"{p.Level}, {p.Hp}");
+                }
+            }
+        }
+
+        static public List<Player> GetHighLevelKnights()
+        {
+            List<Player> players = new List<Player>();
+
+            foreach(Player player in _players)
+            {
+                if (player.ClassType != ClassType.Knight)
+                    continue;
+                if (player.Level < 50)
+                    continue;
+
+                players.Add(player);
+            }
+
+            players.Sort((lhs, rhs) => { return lhs.Level - rhs.Level; });
+            return players;
+        }
+    }
+
+    // Unity 할때는 굳이 LINQ를 사용하지 않고 예전에는 ios관련 버그가 있었음
+    // WebServer에서만 활용
+
+    // Database와 연동해서 데이터를 뽑아온다거나 할 때 MS에서 제품들이 어느정도 호환이됨
+    // 간단하게 LINQ로만 질의를 했더니 실제로 DB에서 데이터를 추출하거나 하는 기술들이 있음
+    // 편하게 사용할 수 있는 기술들이 많이 있음
+}
--- CSstudy/Program.cs
+++ CSstudy/Program.cs
@@ -1,2 +1,92 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+// https://docs.microsoft.com/ko-kr/dotnet/csharp/programming-guide/concepts/async/
+
+// 나중에 Entity Framwork를 다루다보면 async await가 게속등장할 것임
+
+namespace Study
+{ 
+    // async/await 비동기?
+    // 게임 서버> 비동기 = multithread? 꼭 그렇지 않다.
+    // single thread에서도 비동기가 있을 수 있다. 
+    // Unity Coroutine = 일종의 비동기, but single thread
+
+    class Program
+    {
+        static Task Test()
+        {
+            Console.WriteLine("Start Test");
+            // 호출한 순간부터 3초를 셈
+            Task t = Task.Delay(3000);
+            return t;
+        }
+
+        static void TestAsync()
+        {
+            Console.WriteLine("Start TestAsync");
+            // 복잡한 작업 (ex. DB 파일 작업)
+            Task t = Task.Delay(3000);
+
+            // 무식하게 끝날 때까지 기다린다.
+            t.Wait();
+            Console.WriteLine("End TestAsync");
+        }
+
+        // await를 쓰는 경우 반환형이 Task이여야함
+        static async Task TestAsync2() 
+        {
+            Console.WriteLine("Start TestAsync2");
+            // Task t = Task.Delay(3000);
+            // 끝날 때까지 기다리지 않고 바로 제어권을 넘긴다.
+            // await t;
+            // 경우에 따라서 여기가 mutithread로 실행될 수 있다.
+
+            // 한번에 만드는 방법
+            await Task.Delay(3000);
+            Console.WriteLine("End TestAsync2");
+        }
+
+        
+        static async Task<int> TestAsync3()
+        {
+            Console.WriteLine("Start TestAsync3");
+            await Task.Delay(3000);
+            Console.WriteLine("End TestAsync3");
+            return 1;
+        }
+
+        static void main(string[] args)
+        {
+            // Task t = Test();
+            // t.Wait(); // 끝날 때까지 기다릴 수 있다.
+
+            // 이런식으로도 할 수 있다.
+            // TestAsync();
+
+            Console.WriteLine("while start");
+
+            while (true)
+            {
+
+            }
+        }
+
+        static async Task Main(string[] args)
+        {
+            // 대기, main함수를 호출한쪽에 흐름을 넘김 
+            // await TestAsync2();
+
+            // 3초후에 while start가 뜬다.
+            // Console.WriteLine("while start");
+
+            Task<int> t = TestAsync3(); // task를 받은 다음에
+            // 중간에 다른 일도 할 수 있음
+            Console.WriteLine("while start");
+            int ret = await t;
+
+            while (true)
+            {
+
+            }
+        }
+    }
+}
